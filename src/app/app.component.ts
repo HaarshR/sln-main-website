153f465431cd10826bee3938186643cb404b4ea2
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-23 13:56:00 +0400

    Moved footer to footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +6,5 @@ import { faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  faInstagram = faInstagram;
-  faFacebook = faFacebookF;
-
   title = 'sln-website';
 }
