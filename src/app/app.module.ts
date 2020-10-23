54c2dee9d812921133aee18102e4271a773c5be1
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-23 17:42:33 +0400

    Fixed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DepartmentPageComponent } from './department-page/department-page.component';
 import { LandingComponent } from './landing/landing.component';
 
