850c78a5d8ad1dbd453c7e256c9268d9933b04e6
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-23 17:53:56 +0400

    Updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { DepartmentPageComponent } from './department-page/department-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { DepartmentPageComponent } from './department-page/department-page.compo
     NavbarComponent,
     FooterComponent,
     DepartmentPageComponent,
+    LandingPageComponent,
   ],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
