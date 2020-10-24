efd420add1d837c39e2b4b7ac582d7ef2b39dd07
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-24 16:45:16 +0400

    Added title, mission, meet the team, gallery

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faInstagram, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss'],
 })
 export class AboutPageComponent implements OnInit {
+  faInstagram = faInstagram;
+  faFacebook = faFacebookF;
   // Dynamic Content
   backgroundImage: string =
     'https://images.unsplash.com/photo-1539946309076-4daf2ea73899?ixlib=rb-1.2.1&w=1000&q=80';
+
+  members = [1, 2, 3];
 
   constructor() {}
 
