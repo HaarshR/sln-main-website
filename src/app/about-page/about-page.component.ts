7f04068fb7cff412d081d0aa1ad60354c3a99140
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-24 14:41:17 +0400

    Started about pgae

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
-  styleUrls: ['./about-page.component.scss']
+  styleUrls: ['./about-page.component.scss'],
 })
 export class AboutPageComponent implements OnInit {
+  // Dynamic Content
+  backgroundImage: string =
+    'https://images.unsplash.com/photo-1539946309076-4daf2ea73899?ixlib=rb-1.2.1&w=1000&q=80';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
