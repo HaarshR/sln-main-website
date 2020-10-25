d7a1a03ae3fafb2661400d74d15dfbd6472d0a44
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-25 23:46:22 +0400

    Cleanup

--- src/app/landing-page/service-card/service-card.component.ts
+++ src/app/landing-page/service-card/service-card.component.ts
@@ -10,7 +10,7 @@ import { faChevronCircleRight } from '@fortawesome/free-solid-svg-icons';
 export class ServiceCardComponent implements OnInit {
   faChevronCircleRight = faChevronCircleRight;
 
-  backgroundImage: string =
+  backgroundImage =
     'https://images.unsplash.com/photo-1539946309076-4daf2ea73899?ixlib=rb-1.2.1&w=1000&q=80';
 
   constructor() {}
