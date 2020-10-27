4b005a04500df3d96629b36bb935cce343f13caf
MasterSaad <sdmamodesaeb@student.udm.ac.mu>
2020-10-27 14:54:20 +0400

    edited card

--- src/app/joinus-page/joinus-page.component.ts
+++ src/app/joinus-page/joinus-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-regular-svg-icons'
 
 @Component({
   selector: 'app-joinus-page',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./joinus-page.component.scss']
 })
 export class JoinusPageComponent implements OnInit {
+  faUser = faUser;
+  
   backgroundImage: string = //'https://9to5mac.com/wp-content/uploads/sites/6/2014/07/hero_2x.jpg';
     'https://images.unsplash.com/photo-1539946309076-4daf2ea73899?ixlib=rb-1.2.1&w=1000&q=80';
 
