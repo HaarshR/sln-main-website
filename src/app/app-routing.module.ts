71ea313f85d4d81cff7f5b7d1adef9db936a1b34
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-27 14:06:28 +0400

    Updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
 import { AdminHomepageComponent } from './admin/admin-homepage/admin-homepage.component';
+import { BlogPageComponent } from './blog-page/blog-page.component';
+import { EventsPageComponent } from './events-page/events-page.component';
+import { JoinusPageComponent } from './joinus-page/joinus-page.component';
 
 // Components
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'about-us', component: AboutPageComponent },
+  { path: 'blog', component: BlogPageComponent },
+  { path: 'events', component: EventsPageComponent },
+  { path: 'join-us', component: JoinusPageComponent },
   { path: 'admin', component: AdminHomepageComponent },
   // {
   //   path: 'adminHome',
