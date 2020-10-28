dd5af215e938f551fb3cb27fdbe1e0c00b1a4a8f
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-28 23:16:39 +0400

    Added navbar service and page data

--- src/app/joinus-page/joinus-page.component.ts
+++ src/app/joinus-page/joinus-page.component.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+
+import { PageData } from 'src/models/PageData';
+import { NavbarService } from '../shared/navbar/navbar.service';
 
 @Component({
   selector: 'app-joinus-page',
   templateUrl: './joinus-page.component.html',
-  styleUrls: ['./joinus-page.component.scss']
+  styleUrls: ['./joinus-page.component.scss'],
 })
 export class JoinusPageComponent implements OnInit {
+  private pageData: PageData = {
+    navBackground: 'rgba(0,0,0, 0.7)',
+    navLink: '#ffffff',
+    specialNavLink: '#f89825',
+  };
+
   faUser = faUser;
-  
+
   backgroundImage: string = //'https://9to5mac.com/wp-content/uploads/sites/6/2014/07/hero_2x.jpg';
     'https://images.unsplash.com/photo-1539946309076-4daf2ea73899?ixlib=rb-1.2.1&w=1000&q=80';
 
-  constructor() { }
+  constructor(private navbarService: NavbarService) {}
 
   ngOnInit(): void {
+    this.navbarService.setPageData(this.pageData);
   }
-
 }
