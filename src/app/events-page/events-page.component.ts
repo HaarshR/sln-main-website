4d3a355b5955e96ccd35368f774f5feadb206348
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-29 14:43:17 +0400

    Started events page

--- src/app/events-page/events-page.component.ts
+++ src/app/events-page/events-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PageData } from 'src/models/PageData';
+import { NavbarService } from '../shared/navbar/navbar.service';
 @Component({
   selector: 'app-events-page',
   templateUrl: './events-page.component.html',
-  styleUrls: ['./events-page.component.scss']
+  styleUrls: ['./events-page.component.scss'],
 })
 export class EventsPageComponent implements OnInit {
+  private pageData: PageData = {
+    navBackground: '#ffffff',
+    navLink: '#000000',
+    specialNavLink: '#546c55',
+    togglerColor: '#546c55',
+  };
 
-  constructor() { }
+  events = [1, 2, 3];
+
+  constructor(private navbarService: NavbarService) {}
 
   ngOnInit(): void {
+    this.navbarService.setPageData(this.pageData);
   }
-
 }
