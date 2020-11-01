bf36ce9a39b6422dc14afd4e845ca3a68709fc51
MasterSaad <sdmamodesaeb@student.udm.ac.mu>
2020-11-01 16:07:09 +0400

    Slide images
    did images slideshow using online images. Altaaf do not pull now!! NEED TO CHECK FIRST

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
 import { PageData } from 'src/models/PageData';
 import { NavbarService } from '../shared/navbar/navbar.service';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
+  providers: [NgbCarouselConfig],
 })
 export class LandingPageComponent implements OnInit {
+
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
+  images = [1055, 194, 368].map( (n) => `https://picsum.photos/id/${n}/1500/600`);
+
   private pageData: PageData = {
     navBackground: 'rgba(0,0,0,0.7)',
     navLink: '#ffffff',
@@ -18,7 +26,10 @@ export class LandingPageComponent implements OnInit {
   backgroundImage =
     'https://images.unsplash.com/photo-1539946309076-4daf2ea73899?ixlib=rb-1.2.1&w=1000&q=80';
 
-  constructor(private navbarService: NavbarService) {}
+  constructor(private navbarService: NavbarService, config: NgbCarouselConfig) {
+    config.showNavigationArrows = false;
+    config.showNavigationIndicators = false;
+  }
 
   ngOnInit(): void {
     this.navbarService.setPageData(this.pageData);
