d0a3f0f9ce09829ff7ebc47bdd71b6b0dba2d2fa
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-11-01 12:55:07 +0400

    Modified blog page

--- src/app/blog-page/blog-page.component.ts
+++ src/app/blog-page/blog-page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { PageData } from 'src/models/PageData';
+import { NavbarService } from '../shared/navbar/navbar.service';
 
 @Component({
   selector: 'app-blog-page',
   templateUrl: './blog-page.component.html',
-  styleUrls: ['./blog-page.component.scss']
+  styleUrls: ['./blog-page.component.scss'],
 })
 export class BlogPageComponent implements OnInit {
+  private pageData: PageData = {
+    navBackground: '#ffffff',
+    navLink: '#000000',
+    specialNavLink: '#546c55',
+    togglerColor: '#546c55',
+  };
 
-  constructor() { }
+  blogs = [1, 2, 3];
+
+  constructor(private navbarService: NavbarService) {}
 
   ngOnInit(): void {
+    this.navbarService.setPageData(this.pageData);
   }
-
 }
