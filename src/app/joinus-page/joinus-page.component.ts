be8c79eca814ac115c35a1816e0fcb36a768ac54
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-11-02 14:34:32 +0400

    Added new icons and onClick method

--- src/app/joinus-page/joinus-page.component.ts
+++ src/app/joinus-page/joinus-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faUser, faHandshake } from '@fortawesome/free-regular-svg-icons';
 
 import { PageData } from 'src/models/PageData';
 import { NavbarService } from '../shared/navbar/navbar.service';
@@ -18,6 +18,7 @@ export class JoinusPageComponent implements OnInit {
   };
 
   faUser = faUser;
+  faHandshake = faHandshake;
 
   backgroundImage: string = //'https://9to5mac.com/wp-content/uploads/sites/6/2014/07/hero_2x.jpg';
     'https://images.unsplash.com/photo-1539946309076-4daf2ea73899?ixlib=rb-1.2.1&w=1000&q=80';
@@ -26,5 +27,13 @@ export class JoinusPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.navbarService.setPageData(this.pageData);
+  }
+
+  onClick(memberType): void {
+    if (memberType === 'regular') {
+      // navigate to regular form
+    } else {
+      // navigate to executive form
+    }
   }
 }
