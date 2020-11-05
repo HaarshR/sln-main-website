2ddc584224cf86b5a7413f7c0bc38db987f6bcee
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-11-05 14:35:51 +0400

    Made fields dynamic

--- src/app/blog-page/view-blog/view-blog.component.ts
+++ src/app/blog-page/view-blog/view-blog.component.ts
@@ -22,6 +22,15 @@ export class ViewBlogComponent implements OnInit {
 
   comments = [1, 2, 3];
 
+  title = 'An Awesome Title';
+  author = 'someone awesome'.toUpperCase();
+  date = '32 December 2020';
+  subtitle =
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa possimus ipsum dolores eius in delectus nam, itaque neque minus qui, cum asperiores nihil, inventore quo maxime doloribus aliquam totam repellat?';
+
+  body =
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia eligendi dolores soluta. Officia in culpa sequi facilis natus modi eligendi doloribus vitae veritatis iure at quos consequuntur quia, harum sint beatae totam fugit nemo sed expedita aut! Accusantium, sapiente ipsa voluptatem officia nihil amet, aliquid magni placeat optio ipsum,impedit unde! Repellat porro, quibusdam iste nam fuga dolorem iusto minus ullam accusamus aperiam quia facere quidem! Sequi, nam? Fugiat, repellendus. Veritatis labore reprehenderit iure at iusto accusamus mollitia fugit earum impedit exercitationem debitis fuga omnis laborum obcaecati et, quisquam porro ullam blanditiis dolore eaque! Tempora saepe enim facilis hic consequuntur.';
+
   constructor(private navbarService: NavbarService) {}
 
   ngOnInit(): void {
