0ec3e46151fb3af51f3b6be3658f8c2a83586d62
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-11-07 00:19:29 +0400

    Added department card dummy data

--- src/app/department-page/department-page.component.ts
+++ src/app/department-page/department-page.component.ts
@@ -17,7 +17,38 @@ export class DepartmentPageComponent implements OnInit {
     visible: true,
   };
 
-  departments = [1, 2, 3, 4, 5];
+  departments: { title: string; thumbnail: string }[] = [
+    {
+      title: 'Administration',
+      thumbnail:
+        'https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Art',
+      thumbnail:
+        'https://images.unsplash.com/photo-1569172122301-bc5008bc09c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Eco Fashion',
+      thumbnail:
+        'https://images.unsplash.com/photo-1586363104862-3a5e2ab60d99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Fauna',
+      thumbnail:
+        'https://images.unsplash.com/photo-1535941339077-2dd1c7963098?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Flora',
+      thumbnail:
+        'https://images.unsplash.com/photo-1470755008296-2939845775eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+    {
+      title: 'Holistic Education',
+      thumbnail:
+        'https://images.unsplash.com/photo-1550399105-c4db5fb85c18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+    },
+  ];
 
   constructor(private navbarService: NavbarService) {}
 
