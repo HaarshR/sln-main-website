18d49f747d98a2e24453de1a4337b84c11efb961
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-11-06 22:47:41 +0400

    Added upcoming and past events properties

--- src/app/events-page/events-page.component.ts
+++ src/app/events-page/events-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventModel } from 'src/models/EventModel';
 import { PageData } from 'src/models/PageData';
 import { NavbarService } from '../shared/navbar/navbar.service';
 @Component({
@@ -15,7 +16,33 @@ export class EventsPageComponent implements OnInit {
     visible: true,
   };
 
-  events = [1, 2, 3];
+  upcomingEvents: EventModel[] = [
+    {
+      _id: '1',
+      date: new Date('11/07/2020'),
+      departmentName: 'IT Department',
+      name: 'Website Launch',
+      description: "An awesome IT event for the website's launch",
+      location: 'SD Worx',
+      logo:
+        'https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      images: [],
+    },
+  ];
+
+  pastEvents: EventModel[] = [
+    {
+      _id: '1',
+      date: new Date('10/13/2020'),
+      departmentName: 'IT Department',
+      name: 'Website Discussion',
+      description: 'Meeting discussion for a public website',
+      location: 'Zoom Cloud Meetings',
+      logo:
+        'https://images.unsplash.com/photo-1552664730-d307ca884978?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+      images: [],
+    },
+  ];
 
   constructor(private navbarService: NavbarService) {}
 
