ff36571b3e5356a79b20dd75f452d028a717dd94
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-11-07 00:19:50 +0400

    Added department text on card

--- src/app/department-page/department-card/department-card.component.ts
+++ src/app/department-page/department-card/department-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-department-card',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./department-card.component.scss'],
 })
 export class DepartmentCardComponent implements OnInit {
+  @Input() department: { title: string; thumbnail: string };
   constructor() {}
 
   ngOnInit(): void {}
